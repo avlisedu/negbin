2ba8768e9703516fc77b121e4c7dd9bafa1d7e76
Eduardo da Silva <avlis.edu@hotmail.com>
2025-02-03 18:57:11 -0300

    teste

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+# import plotly.express as px
 from linearmodels.panel import PooledOLS, PanelOLS, RandomEffects
 from statsmodels.discrete.count_model import ZeroInflatedNegativeBinomialP
 from patsy import dmatrices
